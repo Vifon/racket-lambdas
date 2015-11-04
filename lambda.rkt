090aa4bf1aecb13b35b97c73d08fd733c7de4c5b
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-11-04 16:15:10 +0100

    Move Y-combinator calls to the function definitions

--- lambda.rkt
+++ lambda.rkt
@@ -21,30 +21,30 @@
             cdr))))
 
 (define my-map
-  (lambda (f)
-    (lambda (thunk lst)
-      (if (equal? lst '())
-          '()
-          (my-cons (thunk (my-car lst))
-                   ((force f) thunk (my-cdr lst)))))))
+  (Y (lambda (f)
+       (lambda (thunk lst)
+         (if (equal? lst '())
+             '()
+             (my-cons (thunk (my-car lst))
+                      ((force f) thunk (my-cdr lst))))))))
 
 (define my-range
-  (lambda (f)
-    (lambda (start end)
-      (if (< start end)
-          (my-cons start
-                   ((force f) (+ start 1) end))
-          '()))))
+  (Y (lambda (f)
+       (lambda (start end)
+         (if (< start end)
+             (my-cons start
+                      ((force f) (+ start 1) end))
+             '())))))
 
 (define fibonacci
-  (lambda (f)
-    (lambda (n)
-      (if (<= n 1)
-          1
-          (+ ((force f) (- n 1))
-             ((force f) (- n 2)))))))
+  (Y (lambda (f)
+       (lambda (n)
+         (if (<= n 1)
+             1
+             (+ ((force f) (- n 1))
+                ((force f) (- n 2))))))))
 
 (void
- ((Y my-map) (lambda (x) (write x) (newline))
-             ((Y my-map) (Y fibonacci)
-                         ((Y my-range) 0 10))))
+ (my-map (lambda (x) (write x) (newline))
+         (my-map fibonacci
+                 (my-range 0 10))))
